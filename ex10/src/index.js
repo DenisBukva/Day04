499e9590da6e8b6fbd19afb6e24bd347542cbc8e
DenisBukva <denisbukva@hotmail.com>
2021-03-11 16:43:39 +0100

    add ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+    var myFirstName = "First name";
+    var myLastName = "Last name";
+    var myHomeTown = "Home Town";
+
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
+    
+
+
+
+
